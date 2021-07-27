262113128450676f1881e0c64dbc0e0dd294970f
Derek DeLizo <delizoderek@gmail.com>
2021-07-26 21:25:56 -0700

    Can now add a department to the database using inquirer input

--- db/query.sql
+++ db/query.sql
@@ -1,10 +1,16 @@
-SELECT
-    e.id,
-    concat(e.first_name," ",e.last_name) AS Employee,
-    role.title,
-    concat(m.first_name," ",m.last_name) AS Manager
-FROM
-    employee e
-LEFT JOIN employee m ON m.id = e.manager_id
-JOIN role ON e.role_id=role.id
-JOIN department ON role.department_id=department.id;
+-- SELECT * FROM department;
+
+-- SELECT role.id, role.title, department.name AS department, role.salary
+-- FROM role
+-- JOIN department ON role.department_id=department.id;
+
+-- SELECT
+--     e.id,
+--     concat(e.first_name," ",e.last_name) AS Employee,
+--     role.title,
+--     concat(m.first_name," ",m.last_name) AS Manager
+-- FROM
+--     employee e
+-- LEFT JOIN employee m ON m.id = e.manager_id
+-- JOIN role ON e.role_id=role.id
+-- JOIN department ON role.department_id=department.id;
